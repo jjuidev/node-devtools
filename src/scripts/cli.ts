1184c6cfd0e93880d8de14a565a371c3e6c984f8
jjuidev <jjuidev@gmail.com>
2025-11-06 21:59:12 +0700

    v1.3.0

--- src/scripts/cli.ts
+++ src/scripts/cli.ts
@@ -5,7 +5,7 @@ import { runInteractiveSetup } from './interactive-setup.js';
 program
 	.name('node-devtools')
 	.description('Interactive devtools (commitlint, eslint, prettier,...) setup for Node.js projects')
-	.version('1.2.0');
+	.version('1.3.0');
 
 program.description('Interactive setup - Install and configure all dev dependencies').action(async () => {
 	await runInteractiveSetup();
